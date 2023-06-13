c3cb7e7c7c80af4c07c866ca26feac56b95b0272
devjsrana <ashrafujjamanrana@gmail.com>
2023-06-13 16:45:56 +0600

    build issue solve

---
+++ carbon-emission/dashboard/src/pages/UnassignedTagsPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const UnassignedTagsPage = (props: Props) => {
+    return (
+        <div>UnassignedTagsPage</div>
+    )
+}
+
+export default UnassignedTagsPage
